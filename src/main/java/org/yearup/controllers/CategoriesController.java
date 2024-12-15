b0c6bc25a060b759cc6ccf645108202304f1a276
Christian <you@example.com>
2024-12-15 02:01:21 -0600

    GET category/id functional

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -31,12 +31,11 @@ public class CategoriesController
         return categoryDao.getAllCategories();
     }
 
-    // add the appropriate annotation for a get action
+
     @GetMapping("{id}")
     public Category getById(@PathVariable int id)
     {
-        // get the category by id
-        return null;
+        return categoryDao.getById(id);
     }
 
     // the url to return all products in category 1 would look like this
